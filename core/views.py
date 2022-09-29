c8d2efe8a3e905bff326259ebeabc5c772cbabd2
Juan Spera <juanspera@hotmail.com>
2022-09-29 02:00:30 +0200

    logout

--- core/views.py
+++ core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def about(request):
     return render(request, 'core/about.html')
 
+@login_required
 def contact(request):
     return render(request, 'core/contact.html')
 
+@login_required
 def users(request):
     return render(request, 'core/users.html')
 
+@login_required
 def index(request):
     return render(request, 'core/base.html')
--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from user.views import *
 urlpatterns = [
     path('login/', login_request, name='UserLogin'),
     path('registro/', register, name='UserRegister'),
-    path('logout/', LogoutView.as_view(template_name='user/templates/logout.html'), name='UserLogout'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='UserLogout'),
     path('editar/', editar_usuario, name='UserEditar'),
     path('avatar/', upload_avatar, name='UserAvatar'),
     path('prueba/', prueba, name='prueba'),
