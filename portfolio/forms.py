19041cb425cf8026e7ed7578a45c0b3cc7fa90f0
Juan Spera <juanspera@hotmail.com>
2022-09-29 15:49:12 +0200

    cked_form

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-
+from ckeditor.widgets import CKEditorWidget
 
 class ProjectFormulario(forms.Form):
     title = forms.CharField(max_length=200, label='Título')
     subtitle = forms.CharField(max_length=200, label='Subtítulo',required=False)
-    description = forms.CharField(label='Comentario')
+    description = forms.CharField(label='Comentario', widget = CKEditorWidget())
     image = forms.ImageField(label='Imagen', required=False)
     link = forms.URLField(required=False, label="Link")
