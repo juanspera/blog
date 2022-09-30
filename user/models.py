48b2bb69ef1b02715a9e054f36b3fc7c7ccb9c06
Juan Spera <juanspera@hotmail.com>
2022-09-30 21:02:30 +0200

    con_posts

--- user/models.py
+++ user/models.py
@@ -4,5 +4,4 @@ from django.db import models
 
 class Avatar(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
-    user = models.CharField(max_length=200)
     imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
